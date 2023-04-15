342d65067558f3fcdeba532f3d727124f7a8a4cd
adnanbayuaji <adnan.bayuaji@gmail.com>
2023-04-15 08:32:18 +0700

    fix bug

--- final-project/router/router.go
+++ final-project/router/router.go
@@ -27,30 +27,30 @@ func StartApp() *gin.Engine {
 	{
 		socialmediaRouter.Use(middlewares.Authentication())
 		socialmediaRouter.GET("/", controllers.GetAllSocialmedia)
-		socialmediaRouter.GET("/:socialmediaID", controllers.GetOneSocialmedia)
+		socialmediaRouter.GET("/:socialmediaId", controllers.GetOneSocialmedia)
 		socialmediaRouter.POST("/", controllers.CreateSocialmedia)
-		socialmediaRouter.PUT("/:socialmediaID", middlewares.SocialmediaAuthorization(), controllers.UpdateSocialmedia)
-		socialmediaRouter.DELETE("/:socialmediaID", middlewares.SocialmediaAuthorization(), controllers.DeleteUser)
+		socialmediaRouter.PUT("/:socialmediaId", middlewares.SocialmediaAuthorization(), controllers.UpdateSocialmedia)
+		socialmediaRouter.DELETE("/:socialmediaId", middlewares.SocialmediaAuthorization(), controllers.DeleteUser)
 	}
 
 	photoRouter := r.Group("/photos")
 	{
 		photoRouter.Use(middlewares.Authentication())
 		photoRouter.GET("/", controllers.GetAllPhoto)
-		photoRouter.GET("/:socialmediaID", controllers.GetOnePhoto)
+		photoRouter.GET("/:photoId", controllers.GetOnePhoto)
 		photoRouter.POST("/", controllers.CreatePhoto)
-		photoRouter.PUT("/:socialmediaID", middlewares.PhotoAuthorization(), controllers.UpdatePhoto)
-		photoRouter.DELETE("/:socialmediaID", middlewares.PhotoAuthorization(), controllers.DeleteUser)
+		photoRouter.PUT("/:photoId", middlewares.PhotoAuthorization(), controllers.UpdatePhoto)
+		photoRouter.DELETE("/:photoId", middlewares.PhotoAuthorization(), controllers.DeleteUser)
 	}
 
 	commentRouter := r.Group("/comments")
 	{
 		commentRouter.Use(middlewares.Authentication())
 		commentRouter.GET("/", controllers.GetAllComment)
-		commentRouter.GET("/:socialmediaID", controllers.GetOneComment)
-		commentRouter.POST("/:photoID", controllers.CreateComment)
-		commentRouter.PUT("/:socialmediaID/:photoID", middlewares.CommentAuthorization(), controllers.UpdateComment)
-		commentRouter.DELETE("/:socialmediaID", middlewares.CommentAuthorization(), controllers.DeleteUser)
+		commentRouter.GET("/:commentId", controllers.GetOneComment)
+		commentRouter.POST("/:photoId", controllers.CreateComment)
+		commentRouter.PUT("/:commentId/:photoId", middlewares.CommentAuthorization(), controllers.UpdateComment)
+		commentRouter.DELETE("/:commentId", middlewares.CommentAuthorization(), controllers.DeleteUser)
 	}
 
 	return r
